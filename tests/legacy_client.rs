85f8c48a8f0225619d1f8f10c0e38c156a71c41b
Zelda Hessler <zhessler@amazon.com>
2024-05-29 10:15:17 -0500

    cargo fmt

--- tests/legacy_client.rs
+++ tests/legacy_client.rs
@@ -137,7 +137,11 @@ async fn drop_client_closes_idle_connections() {
     // not closed yet, just idle
     future::poll_fn(|ctx| {
         let poll_res = Pin::new(&mut closes).poll_next(ctx);
-        assert!(poll_res.is_pending(), "expected pending poll but got {:?}", poll_res);
+        assert!(
+            poll_res.is_pending(),
+            "expected pending poll but got {:?}",
+            poll_res
+        );
         Poll::Ready(())
     })
     .await;
